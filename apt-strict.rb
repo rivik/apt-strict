5ac697fe99c1a3e384a2097ef698166309752420
Ilya Rusalowski <rivik@yandex-team.ru>
2015-09-07 20:34:02 +0300

    fix

--- apt-strict.rb
+++ apt-strict.rb
@@ -3,16 +3,15 @@ require "formula"
 class AptStrict < Formula
   homepage "https://github.com/selivan/apt-strict"
   url "git@github.com:rivik/apt-strict.git", :using => :git
-  version "0.5-5"
+  version "0.5-6"
 
   depends_on "ansible"
 
   def install
-        (share/'apt_strict').install 'apt_strict.py' 
         (share/'apt_strict').install 'executable.py' 
+        (share/'apt_strict').install 'apt_strict.py' 
+        (share/'ansible/packaging').install 'apt_strict.py' => 'apt_strict'
         (etc/'bash_completion.d').install 'bash_completion.d/apt-strict'
         bin.install_symlink (share/'apt_strict/executable.py') => 'apt-strict'
-        mkdir_p (share/'ansible/packaging')
-        (share/'ansible/packaging').install_symlink (share/'apt_strict/apt_strict.py') => 'apt_strict'
   end
 end
