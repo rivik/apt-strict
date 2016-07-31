21ca6f0621b5ed321ef0423659874399677bca82
Ilya Rusalowski <rivik@yandex-team.ru>
2016-08-01 01:33:55 +0300

    ansible19 optional dependency

--- apt-strict.rb
+++ apt-strict.rb
@@ -3,9 +3,10 @@ require "formula"
 class AptStrict < Formula
   homepage "https://github.com/selivan/apt-strict"
   url "git@github.com:rivik/apt-strict.git", :using => :git
-  version "0.5-10"
+  version "0.5-11"
 
-  depends_on "ansible"
+  depends_on "ansible" => :optional
+  depends_on "ansible19" => :optional
 
   def install
         # no apt for max os x =), install ansible module only
